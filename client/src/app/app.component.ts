7f68ad5783a4bea41c17f98c6b911789edb60cac
Michael ilkanayev <93651794+michaelilkanayev1997@users.noreply.github.com>
2024-08-30 00:17:15 +0300

    Added PRIMENG

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
 import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
